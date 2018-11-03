c061b3fd793283100368c60184f54561ece6a6d7
Bailey Pelletier <baileypelletier@yahoo.com>
2018-11-02 22:45:38 -0400

    Updated Matrix library
    Added matrix operations and some derivations as well as included Identity Matrix option in constructors

--- ToyNN/src/Matrix.java
+++ ToyNN/src/Matrix.java
@@ -6,7 +6,7 @@
  *
  */
 
-
+import java.text.DecimalFormat;
 public class Matrix {
 	
 	//Instance Variables
@@ -14,7 +14,39 @@ public class Matrix {
 	private int cols;
 	private double[][] data;
 	
-	//Constructors
+	//Constructors & Related
+	/**
+	 * Constructs a Zero matrix with the given parameters.
+	 * Will construct a matrix with m leading ones with m
+	 * representing the number of rows in an mxn matrix. 
+	 *
+	 * @param rows	Number of rows
+	 * @param cols	Number of columns
+	 * @param ident	Sets if constructor builds identity matrix
+	 */
+	public Matrix (int rows, int cols, boolean ident) {
+		this.setRows(rows);
+		this.setCols(cols);
+		data = new double[rows][cols];
+		
+		if (ident) {
+			for (int i = 0; i < rows; i++) {
+				for (int j = 0; j < cols; j++) {
+					if (i == j) {
+						this.setElement(i, j, 1.0);
+					} else {
+						this.setElement(i, j, 0.0);
+					}
+				}
+			}
+		} else {
+			for (int i = 0; i < rows; i++) {
+				for (int j = 0; j < cols; j++) {
+					this.setElement(i, j, 0.0);
+				}
+			}
+		}
+	}
 	/**
 	 * Constructs a Zero matrix with the given parameters 
 	 *
@@ -22,19 +54,17 @@ public class Matrix {
 	 * @param cols	Number of columns
 	 */
 	public Matrix (int rows, int cols) {
-		this.setRows(rows);
-		this.setCols(cols);
-		
-		data = new double[rows][cols];
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-				this.setElement(i, j, 0.0);
-			}
-		}
+		this(rows, cols, false);
 	}
 	
+	/**
+	 * Returns a vector matrix based on a 1D Array 
+	 *
+	 * @param arr		1D Array to be converted to column vector
+	 * @return matrix	Column vector represented as a matrix (nx1)
+	 */
 	public static Matrix fromArrary(double[] arr) {
-		Matrix mat = new Matrix(arr.length, 1);
+ 		Matrix mat = new Matrix(arr.length, 1);
 		for (int i = 0; i < mat.getRows(); i++) {
 			mat.setElement(i, 0, arr[i]);
 		}
@@ -42,15 +72,33 @@ public class Matrix {
 	}
 	
 	/**
+	 * Returns an array based on a column vector 
+	 *
+	 * @param a				Column vector matrix (nx1)
+	 * @return arr[]		1D Array
+	 */
+	public static double[] toArrary(Matrix a) {
+		double[] arr = new double[a.getRows()];
+		
+		for (int i = 0; i < a.getRows(); i++) {
+			arr[i] = a.getElement(i, 0);
+		}
+		
+		return arr;
+	}
+	/**
 	 * Returns a String representation of a matrix object
 	 * 
 	 * @return string	String representing each element
 	 */
 	public String toString() {
+		DecimalFormat decimalFormatObj = (DecimalFormat) DecimalFormat.getInstance();
+        decimalFormatObj.setMinimumFractionDigits(3);
+        decimalFormatObj.setMaximumFractionDigits(3);
 		String s = "";
 		for (int i = 0; i < this.rows; i++) {
 			for (int j = 0; j < cols; j++) {
-				s += this.data[i][j] + "\t";
+				s += decimalFormatObj.format(this.data[i][j]) + "\t";
 			}
 			s += "\n";
 		}
@@ -69,7 +117,18 @@ public class Matrix {
 			}
 		}
 	}
-	
+	/**
+	 * Initializes each value of a matrix between 0 and 10
+	 * Truncated when casted to ints
+	 * 
+	 */
+	public void initialRandInt() {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				this.setElement(i, j, ((int)(Math.random()*10)));
+			}
+		}
+	}
 	/**
 	 * Initializes each value of a matrix starting at 1 and counting up
 	 * 
@@ -134,10 +193,14 @@ public class Matrix {
 	 * @return det The determinant of a 2x2 matrix
 	 */
 	public double getDeterm2x2() {
-		double det = 0.123;
+		double det = 0.0;
 		
-		det = (this.getElement(0,0) * this.getElement(1, 1)) - (this.getElement(0, 1) * this.getElement(1, 0));
-		
+		if (this.isSqaure() && this.getRows() == 2) {
+			det = (this.getElement(0,0) * this.getElement(1, 1)) - (this.getElement(0, 1) * this.getElement(1, 0));
+		} else {
+			System.out.println("This matrix is not square or is not a 2x2 matrix!\n"
+					+ "Returning 0.0 as determinant...");
+		}
 		
 		return det;
 	}
@@ -150,6 +213,7 @@ public class Matrix {
 	public double getDeterm() {
 		double det = 0.0;
 				
+		if (this.isSqaure()) {
 			if(this.rows == 2) {
 				det = getDeterm2x2();
 				
@@ -162,6 +226,10 @@ public class Matrix {
 					sign *= -1;
 				}
 			}
+		} else {
+			System.out.println("This matrix is not square!\n"
+					+ "Returning 0.0 as determinant...");
+		}
 		
 		
 		
@@ -197,7 +265,51 @@ public class Matrix {
 		return b;
 	}
 	
-	
+	/**
+	 * Returns a transposed matrix based on calling object
+	 * 
+	 * @return matrix	Transposed matrix
+	 */
+	public Matrix getTranspose() {
+		Matrix trans = new Matrix(this.getCols(), this.getRows());
+		
+		for (int j = 0; j < this.getCols(); j++) {
+			for (int i = 0; i < this.getRows(); i++) {
+				trans.setElement(j, i, this.getElement(i, j));
+			}
+		}
+		
+		return trans;
+	}
+
+	/**
+	 * Returns the inverse of the calling Matrix object
+	 * 
+	 * @return matrix	Inversed matrix
+	 */
+	public Matrix getInverse() {
+		Matrix inverse = new Matrix(this.getRows(), this.getCols());
+		
+		if (this.isSqaure() && this.getDeterm() != 0) {
+			
+			if (this.getRows() == 2) {
+				inverse.setElement(0, 0, this.getElement(1, 1));
+				inverse.setElement(0, 1, -this.getElement(0, 1));
+				inverse.setElement(1, 0, -this.getElement(1, 0));
+				inverse.setElement(1, 1, this.getElement(0, 0));
+				
+				inverse.mult(1/this.getDeterm());
+			}
+			
+			
+		} else {
+			System.out.println("This matrix is not invertible!\n"
+					+ "Returning a Zero " + inverse.getRows() + "x" + inverse.getCols() + " Matrix...");
+		}
+		
+		return inverse;
+	}
+
 	//Setters
 	
 	/**
@@ -232,65 +344,159 @@ public class Matrix {
 
 	//Matrix Operations
 
+	/**
+	 * Adds elements of two matrices with the same
+	 * dimensions and returns the sum.
+	 * 
+	 * @param a 		Row index
+	 * @param b 		Col index
+	 * @return matrix 	Value to set to
+	 */
+	public static Matrix add(Matrix a, Matrix b) {
+		Matrix c = new Matrix(a.getRows(),a.getCols());
+		
+		if (a.getRows() == b.getRows() && a.getCols() == b.getCols()) {
+			for (int i = 0; i < a.getRows(); i++) {
+				for (int j = 0; j < a.getCols(); j++) {
+					c.setElement(i, j, (a.getElement(i, j) + b.getElement(i, j)));
+				}
+			}
+		} else {
+			System.out.println("Matrices don't have the same dimensions! \n"
+					+ "Returning zero matrix with dimensions of matrix A...");
+		}
+		
+		return c;
+	}
+	/**
+	 * Subtracts elements of two matrices with the same
+	 * dimensions and returns the sum.
+	 * c = (a - b)
+	 * 
+	 * @param a 		Row index
+	 * @param b 		Col index
+	 * @return matrix 	Value to set to
+	 */
+	public static Matrix subtract(Matrix a, Matrix b) {
+		Matrix c = new Matrix(a.getRows(),a.getCols());
+		
+		if (a.getRows() == b.getRows() && a.getCols() == b.getCols()) {
+			for (int i = 0; i < a.getRows(); i++) {
+				for (int j = 0; j < a.getCols(); j++) {
+					c.setElement(i, j, (a.getElement(i, j) - b.getElement(i, j)));
+				}
+			}
+		} else {
+			System.out.println("Matrices don't have the same dimensions! \n"
+					+ "Returning zero matrix with dimensions of matrix A...");
+		}
+		
+		return c;
+	}
+	/**
+	 * Adds a certain number to each element in a matrix
+	 * 
+	 * @param amount	Amount to add to each cell
+	 */
+	public void add(double amount) {
+		for (int i = 0; i < this.getRows(); i++) {
+			for (int j = 0; j < this.getCols(); j++) {
+				this.setElement(i, j, (this.getElement(i, j) + amount));
+			}
+		}
+	}
 
+	/**
+	 * Multiplies every entry by a scalar
+	 * 
+	 * @param scalar	Scalar to multiply by
+	 */
+	public void mult(double scalar) {
+		for (int i = 0; i < this.getRows(); i++) {
+			for (int j = 0; j < this.getCols(); j++) {
+				this.setElement(i, j, (this.getElement(i, j) * scalar));
+			}
+		}
+	}
+	
+	/**
+	 * Returns the Hadamard product of two matricies.
+	 * Each element is multiplied by its respective element
+	 * of the other matrix
+	 * 
+	 * @param a	1st Matrix
+	 * @param b 2nd Matrix
+	 * @return matrix Hadamard Product
+	 */
+	public static Matrix mult(Matrix a, Matrix b) {
+		Matrix c = new Matrix(a.getRows(),a.getCols());
+		
+		if (a.getRows() == b.getRows() && a.getCols() == b.getCols()) {
+			for (int i = 0; i < a.getRows(); i++) {
+				for (int j = 0; j < a.getCols(); j++) {
+					c.setElement(i, j, (a.getElement(i, j) * b.getElement(i, j)));
+				}
+			}
+		} else {
+			System.out.println("Matrices don't have the same dimensions! \n"
+					+ "Returning zero matrix with dimensions of matrix A...");
+		}
+		
+		return c;
+	}
+	
+	/**
+	 * Returns the dot product between mxn and nxk
+	 * matricies. # of columns of first matrix must
+	 * match # of rows second matrix.
+	 * 
+	 * @param a	1st Matrix
+	 * @param b 2nd Matrix
+	 * @return matrix Dot product
+	 */
+	public static Matrix dot(Matrix a, Matrix b) {
+		Matrix c = new Matrix(a.getRows(),b.getCols());
+				
+		if (a.getCols() == b.getRows()) {
+			
+			for (int i = 0; i < c.getRows(); i++) {
+				for (int j = 0; j < c.getCols(); j++) {
+					
+					double cij = 0.0;
+					
+					for (int k = 0; k < a.getCols(); k++) {
+						cij += a.getElement(i, k) * b.getElement(k, j);
+					}
 
-	
-	
-	
-	
+					//Now in c(ij)
+					c.setElement(i, j, cij);
+					
+				}
+			}
+			
+		} else {
+			System.out.println("Matrices don't have the correct dimensions! \n"
+					+ "Returning zero matrix with dimensions (" + a.getRows() + "x" + b.getCols() + ")...");
+		}
+		
+		return c;
+	}
+
 	
 	
 	
 	
 	public static void main(String[] args) {
-		/*Matrix a = new Matrix(3,3);
-		a.initialRand();
+		
+		Matrix a = new Matrix(2,2);
+		a.initialRandInt();
+		
 		System.out.println(a.toString());
-
+		System.out.println("Determinant: " + a.getDeterm() + "\n");
 		
-		System.out.println("Matrix a has " + a.getRows() + " row(s) and "
-				+ a.getCols() + " column(s).");
-		if (a.isSqaure()) {
-			System.out.println("Matrix a is square");
-		} else {
-			System.out.println("Matrix a is not square");
-		}
-		System.out.println();
+		Matrix aInv = a.getInverse();
+		System.out.println(aInv.toString());
 		
-		System.out.println("The value at 0,0 is: " + a.getElement(0, 0));
-		System.out.println("The value at 0,0 will be changed to 5.999.");
-		a.setElement(0, 0, 5.999);
-		System.out.println("The new value at 0,0 is: " + a.getElement(0, 0));
-		System.out.println(a.toString());
-		*/
-		
-//		Matrix b = new Matrix(2,2);
-//		b.setElement(0, 0, 1);
-//		b.setElement(0, 1, 2);
-//		b.setElement(1, 0, 3);
-//		b.setElement(1, 1, 4);
-//		double detr = b.getDeterm();
-//		
-//		System.out.println(b.toString());
-//		System.out.println(b.isSqaure());
-//		System.out.println(detr);
-		
-//		Matrix orig = new Matrix(3,3);
-//		orig.initialRand();
-//		System.out.println(orig.toString());
-		
-//		Matrix minor = orig.getMinorMatrix(1, 0);
-//		System.out.println(minor.toString());
-		
-//		System.out.println("The determinant is: " + orig.getDeterm());
-//		
-//		Matrix vect = new Matrix(3,1);
-//		vect.initialSimple();
-//		System.out.println(vect.toString());
-		
-		double[] horVect = {3,7,2};
-		Matrix vector = Matrix.fromArrary(horVect);
-		System.out.println(vector.toString());
 		
 	}
 }
